a1a988ffb4bfe42215020319132f7aa7477fe7df
Oscar Davila <oscar7822002@gmail.com>
2021-02-23 14:27:33 -0500

    Firebase hosting

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -5,7 +5,7 @@ import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'registro', component: RegisterComponent },
+  // { path: 'registro', component: RegisterComponent },
 ];
 
 @NgModule({
